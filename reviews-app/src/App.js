704782357e1587b6a21f10b0ee336e54a4e2cb87
Alex <alexbachurin91@gmail.com>
2021-11-26 17:19:18 +0300

    reviews go to next/prev slide

--- reviews-app/src/App.js
+++ reviews-app/src/App.js
@@ -10,6 +10,31 @@ function App() {
   const [index, setIndex] = useState(0);
   //store person based on index
   const person = reviews[index];
+
+
+  //helper function to check for index boundaries
+  const checkIndex = (num) => {
+    //if we about to go out of array length return 0 and go to first slide
+    //else go to last slide
+    if (num > reviews.length - 1) {
+      return 0;
+    } else if (num < 0) {
+      return num = reviews.length - 1;
+    }
+    return num;
+  }
+
+  //change review slides
+  const prevReview = () => {
+    setIndex((index) => {
+      return checkIndex(index - 1);
+    })
+  }
+  const nextReview = () => {
+    setIndex((index) => {
+      return checkIndex(index + 1);
+    })
+  }
   return (
     <main>
       <section className="container">
@@ -26,8 +51,8 @@ function App() {
           <p className="job">{person.job}</p>
           <p className="info">{person.text}</p>
           <div className="button-container">
-            <button className="prev-btn"><GoChevronLeft /></button>
-            <button className="next-btn"><GoChevronRight /></button>
+            <button onClick={prevReview} className="prev-btn"><GoChevronLeft /></button>
+            <button onClick={nextReview} className="next-btn"><GoChevronRight /></button>
           </div>
           <button className="random-btn">Random review</button>
         </article>
