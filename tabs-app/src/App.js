d21b993c0c795b9d7f45a1b44de3380ef02348ec
Alex <alexbachurin91@gmail.com>
2021-12-12 17:38:49 +0300

    tabs-app display 1 tab

--- tabs-app/src/App.js
+++ tabs-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BsChevronDoubleRight } from 'react-icons/bs'
 const url = 'https://course-api.com/react-tabs-project'
 
 
@@ -33,10 +34,31 @@ function App() {
     )
   }
 
+  //get 1 tab to display
+  const { id, order, title, dates, duties, company } = tabs[value];
   return (
-    <div className="App">
-      Tabs App
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>Tabs App</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container"></div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((item, index) => {
+            return (
+              <div className="job-desc">
+                <BsChevronDoubleRight />
+                <p>{item}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
   );
 }
 
