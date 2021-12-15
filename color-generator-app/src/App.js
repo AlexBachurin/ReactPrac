60a8bec9235ad3321c86d9e47ae80a0c46cb4458
Alex <alexbachurin91@gmail.com>
2021-12-16 00:19:39 +0300

    color-generator-app displaying error msg and removing it after few secs

--- color-generator-app/src/App.js
+++ color-generator-app/src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [color, setColor] = useState('');
   //state for list of colors based on input from values.js
   const [colorsList, setColorsList] = useState([]);
+  //state for error
+  const [error, setError] = useState(false);
 
   //handle user input of color
   const handleChange = (e) => {
@@ -22,8 +24,10 @@ function App() {
       //set all tints and shades of this color to our state list
       setColorsList(dataColor.all(10));
       console.log(colorsList)
+      setError(false)
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      setError(true);
     }
   }
   //show default color on load
@@ -31,14 +35,25 @@ function App() {
     const dataColor = new Values('#f15025');
     setColorsList(dataColor.all(10));
   }, [])
+  //useeffect to remove error msg
+  useEffect(() => {
+    const errorId = setTimeout(() => {
+      setError(false)
+    }, 3000);
+    return () => {
+      clearTimeout(errorId)
+    }
+  }, [error])
   return (
     <>
       <section className="container">
         <h3>Color generator</h3>
         <form>
-          <input onChange={handleChange} type="text" placeholder="#f15025" value={color} />
+          <input className={`${error ? 'input-err' : null}`} onChange={handleChange} type="text" placeholder="#f15025" value={color} />
           <button onClick={handleSubmit} type="submit" className="btn">Generate</button>
+          {error ? <ErrorComp /> : null}
         </form>
+
       </section>
       <section className="colors">
         {colorsList.map((color, index) => {
@@ -47,6 +62,12 @@ function App() {
       </section>
     </>
   );
+}
+
+const ErrorComp = () => {
+  return (
+    <h3 className="error-msg">Please enter correct value</h3>
+  )
 }
 
 export default App;
