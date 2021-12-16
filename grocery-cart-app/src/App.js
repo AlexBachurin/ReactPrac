5ff8425201c32bb6a7a9a26fe23289f292b7a2d9
Alex <alexbachurin91@gmail.com>
2021-12-16 23:26:42 +0300

    grocery-cart-app add to list functionality

--- grocery-cart-app/src/App.js
+++ grocery-cart-app/src/App.js
@@ -1,18 +1,46 @@
 import React, { useState } from 'react'
 import List from './components/List';
 const App = () => {
+  //state for input
   const [name, setName] = useState('');
+  //state for list
+  const [list, setList] = useState([]);
+  //state for alert
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  //state for editing item on list
+  const [isEditing, setIsEditing] = useState(false)
+  //handle Submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //check if we have something in input if not show alert
+    if (!name) {
+      setAlert({ show: true, msg: 'please write something first', type: 'danger' })
+    }
+    //handle edit
+    else if (isEditing) {
+
+    }
+    //if all ok add to list
+    else {
+      //create new item and add uniq id to it so we can edit it later
+      const newItem = { id: new Date().getTime().toString(), value: name }
+      setList([...list, newItem])
+      setAlert({ show: true, msg: 'item sucessfully added', type: 'success' })
+    }
+  }
+
   return (
     <section className='section-center'>
-      <form className='grocery-form'>
+      <form onSubmit={handleSubmit} className='grocery-form'>
+        {alert ? <p className={`alert alert-${alert.type}`}>{alert.msg}</p> : null}
         <h3>Grocery cart</h3>
         <div className="form-control">
-          <input className='grocery' type="text" placeholder='enter value' value={name} />
+          <input onChange={(e) => setName(e.target.value)} className='grocery' type="text" placeholder='enter value' value={name} />
           <button type='submit' className='submit-btn'>add</button>
         </div>
       </form>
       <div className="grocery-container">
-        <List />
+        <List list={list} />
       </div>
     </section>
   )
--- grocery-cart-app/src/components/List.js
+++ grocery-cart-app/src/components/List.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa'
-const List = () => {
+const List = ({ list }) => {
     return (
-        <article className='grocery-item'>
-            <p className="title">qwewqeq</p>
-            <div className="btn-container">
-                <button className="edit-btn">
-                    <FaPencilAlt />
-                </button>
-                <button className="delete-btn">
-                    <FaTrashAlt />
-                </button>
-            </div>
-        </article>
+        <div className='grocery-list'>
+            {list.map(item => {
+                return (
+                    <article key={item.id} className='grocery-item'>
+                        <p className="title">{item.value}</p>
+                        <div className="btn-container">
+                            <button className="edit-btn">
+                                <FaPencilAlt />
+                            </button>
+                            <button className="delete-btn">
+                                <FaTrashAlt />
+                            </button>
+                        </div>
+                    </article>
+                )
+            })}
+        </div>
     )
 }
 
