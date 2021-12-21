ea23a7bf0a5b6c3d2dd778b85690d070c17764f2
Alex <alexbachurin91@gmail.com>
2021-12-21 23:36:20 +0300

    submenus-app style adjustments for showing links in open submenu

--- submenus-app/src/components/Submenu.js
+++ submenus-app/src/components/Submenu.js
@@ -1,19 +1,30 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from '../context'
 const Submenu = () => {
     const { sublinks, submenuPage, isSubmenuOpen, location } = useGlobalContext();
     //useRef to style container with proper location
     const submenuContainerRef = useRef(null);
+    //set columns in submenu depending on how many links we have
+    const [cols, setCols] = useState('col-2')
+    const { page, links } = submenuPage;
     //use useEffect to calculate location to show submenu bottom of hovered page and do it every time page/location changes
     useEffect(() => {
+        //set default columns to avoid bugs
+        setCols('col-2');
         submenuContainerRef.current.style.left = `${location.center}px`;
         submenuContainerRef.current.style.bottom = `${location.top}px`;
+        //Set columns based on links length
+        if (links.length > 3) {
+            setCols('col-4')
+        } else if (links.length > 2) {
+            setCols('col-3')
+        }
     }, [location, submenuPage])
-    const { page, links } = submenuPage;
+
     return (
         <aside ref={submenuContainerRef} className={`submenu ${isSubmenuOpen ? 'show' : null}`}>
             <h4>{page}</h4>
-            <div className="submenu-center">
+            <div className={`submenu-center ${cols}`}>
                 {links.map((link, index) => {
                     const { label, url, icon } = link;
                     return (
