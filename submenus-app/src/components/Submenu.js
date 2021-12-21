60fce2c2c883467cc5bd1ab979bf7412cf44b6e1
Alex <alexbachurin91@gmail.com>
2021-12-21 23:30:00 +0300

    submenus-app dynamically displaying info of current hovered submenu

--- submenus-app/src/components/Submenu.js
+++ submenus-app/src/components/Submenu.js
@@ -6,21 +6,21 @@ const Submenu = () => {
     const submenuContainerRef = useRef(null);
     //use useEffect to calculate location to show submenu bottom of hovered page and do it every time page/location changes
     useEffect(() => {
-        console.log(submenuContainerRef)
         submenuContainerRef.current.style.left = `${location.center}px`;
         submenuContainerRef.current.style.bottom = `${location.top}px`;
     }, [location, submenuPage])
+    const { page, links } = submenuPage;
     return (
         <aside ref={submenuContainerRef} className={`submenu ${isSubmenuOpen ? 'show' : null}`}>
-            {/* <h4>{submenuPage.page}</h4>
+            <h4>{page}</h4>
             <div className="submenu-center">
-                {submenuPage.links.map((link, index) => {
+                {links.map((link, index) => {
                     const { label, url, icon } = link;
                     return (
                         <a key={index} href={url}>{icon}{label}</a>
                     )
                 })}
-            </div> */}
+            </div>
         </aside>
     )
 }
