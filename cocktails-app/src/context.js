cfd16ffd044dcf85ca0e286f508aa8e2a2e12a1f
Alex <alexbachurin91@gmail.com>
2021-12-22 11:53:49 +0300

    cocktails-app fetch drinks

--- cocktails-app/src/context.js
+++ cocktails-app/src/context.js
@@ -1,14 +1,45 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 
 
 const AppContext = React.createContext();
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 
 const AppProvider = ({ children }) => {
+    //state for loading
+    const [loading, setLoading] = useState(false)
+    //state for drinks from data
+    const [cocktails, setCocktails] = useState('');
+    //state for search term
+    const [term, setTerm] = useState('');
+    //ref for input
+    const inputRef = useRef(null);
+
+    //helper to fetch
+    const getCocktails = async () => {
+        try {
+            setLoading(true)
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+            setCocktails(data);
+            setLoading(false);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    //fetch drinks on page load
+    useEffect(() => {
+        getCocktails();
+        console.log(cocktails)
+    }, [])
     return (
         <AppContext.Provider value={
             {
-
+                loading,
+                cocktails,
+                term,
+                inputRef
             }
         }>
             {children}
