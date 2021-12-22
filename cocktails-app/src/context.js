5b791a4543bce4a52423712aa489703d37bb8127
Alex <alexbachurin91@gmail.com>
2021-12-22 11:21:13 +0300

    cocktails-app useContext setup

---
+++ cocktails-app/src/context.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useContext } from "react";
+
+
+const AppContext = React.createContext();
+
+
+const AppProvider = ({ children }) => {
+    return (
+        <AppContext.Provider value={
+            {
+
+            }
+        }>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+//custom hook for better use
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider };
