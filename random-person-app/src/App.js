e0b03ad4f4bfc2fe1bbd42c6492fbd0fcdcac965
Alex <alexbachurin91@gmail.com>
2021-12-24 20:16:45 +0300

    random person-app fetch user

--- random-person-app/src/App.js
+++ random-person-app/src/App.js
@@ -9,6 +9,31 @@ import {
 } from 'react-icons/fa'
 const url = 'https://randomuser.me/api/'
 function App() {
+
+  const [user, setUser] = useState({});
+
+  const fetchRandomUser = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    //get user info from data
+    const userInfo = data.results[0];
+    console.log(userInfo);
+    //destructure properties from user we get and add it to state
+    const { first, last } = userInfo.name;
+    const email = userInfo.email;
+    const { age } = userInfo.dob;
+    const phone = userInfo.phone;
+    const { password } = userInfo.login;
+    const { large: image } = userInfo.picture;
+    const { number, name } = userInfo.location.street;
+    const person = { name: `${first} ${last}`, email, phone, age, password, image, street: `${number} ${name}` };
+    setUser(person);
+    console.log(person);
+  }
+  useEffect(() => {
+    fetchRandomUser();
+  }, [])
   return (
     <main>
       <div className="block bcg-black"></div>
