6cd03e693129d6da992151fd99488f2d26357400
Alex <alexbachurin91@gmail.com>
2021-12-27 00:20:38 +0300

    pagination-app prev and next buttons functionality

--- pagination-app/src/App.js
+++ pagination-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Follower from "./components/Follower";
 import Loading from "./components/Loading";
 import useFetch from "./useFetch";
+import paginate from "./utils";
 function App() {
   const { data, loading } = useFetch();
   //state for page
@@ -9,12 +10,30 @@ function App() {
   //state for follower subarray based on current page
   const [followersOnPage, setFollowersOnPage] = useState([]);
 
-  //Click on page button
+  //  ***Click on page button***
   const handlePageChange = (e) => {
     const id = e.target.id;
     setPage(id);
   }
-
+  // ***NEXT AND PREV BTNS***
+  const nextPage = () => {
+    setPage(oldPage => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    })
+  }
+  const prevPage = () => {
+    setPage(oldPage => {
+      let prevPage = oldPage - 1;
+      if (prevPage <= 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    })
+  }
   //check if loading then return, only when we finished loading use paginate functionality
   //thats why we add loading into dependecy array
   useEffect(() => {
@@ -40,14 +59,14 @@ function App() {
         }
         {loading ? null :
           <div className="btn-container">
-            <button className="prev-btn">Prev</button>
+            <button onClick={prevPage} className="prev-btn">Prev</button>
             {/* iterate through followers array and display every page as button index + 1 */}
             {data.map((item, index) => {
               return (
                 <button id={index} onClick={handlePageChange} key={index} className="page-btn">{index + 1}</button>
               )
             })}
-            <button className="next-btn">Next</button>
+            <button onClick={nextPage} className="next-btn">Next</button>
           </div>}
       </section>
     </main>
