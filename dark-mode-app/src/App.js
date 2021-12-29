dc9209b7e8c21fdd9d2c9d52947b53c3d1ea272f
Alex <alexbachurin91@gmail.com>
2021-12-29 23:19:15 +0300

    dark-mode-app initial setup

---
+++ dark-mode-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>dark mode app</h2>
+    </div>
+  );
+}
+
+export default App;
