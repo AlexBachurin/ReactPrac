b2e812b18de04d5ad509aad8ae433dd809ea0af1
Alex <alexbachurin91@gmail.com>
2021-12-30 02:10:16 +0300

    working theme change functionality

--- dark-mode-app/src/App.js
+++ dark-mode-app/src/App.js
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from "react";
 import data from './data'
 import Article from "./components/Article";
 function App() {
+  //state for current theme 
+  const [theme, setTheme] = useState('light-theme');
+
+  useEffect(() => {
+    //every time we change theme, add class to whole document page
+    document.documentElement.className = theme;
+
+  }, [theme])
+
+  //theme change
+  const handleChange = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    } else {
+      setTheme('light-theme');
+    }
+  }
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn">toggle</button>
+          <button onClick={handleChange} className="btn">toggle</button>
         </div>
       </nav>
       <section className="articles">
