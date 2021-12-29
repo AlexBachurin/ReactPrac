bbf3426174eca72721e2f0927d2c96025da3d985
Alex <alexbachurin91@gmail.com>
2021-12-30 02:13:42 +0300

    dark-mode-app save current theme to local storage

--- dark-mode-app/src/App.js
+++ dark-mode-app/src/App.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import data from './data'
 import Article from "./components/Article";
+
+const getLocalStorage = () => {
+  //default theme
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+}
 function App() {
   //state for current theme 
-  const [theme, setTheme] = useState('light-theme');
+  const [theme, setTheme] = useState(getLocalStorage());
 
   useEffect(() => {
     //every time we change theme, add class to whole document page
     document.documentElement.className = theme;
-
+    //and set current theme to local storage
+    localStorage.setItem('theme', theme)
   }, [theme])
 
   //theme change
